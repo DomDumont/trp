ba96e1783dbac858ccc3b231d344c10e7082f9c4
MeyerAtWork <meyer.dumont@gmail.com>
2014-09-15 16:53:39 +0200

    Update Visual version + better batch to create a win32 version

--- rototo/tools/prepare_version.bat
+++ rototo/tools/prepare_version.bat
@@ -1,20 +1,32 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-msbuild ..\projects\Visual2010\Rototo.sln /property:Configuration=Release
+REM ----------------------------------------------------------------------------
+REM  Build Documentation
+REM ----------------------------------------------------------------------------
 
-cd .\doc
-python build_doc.py
-cd ..
+PUSHD .\doc
+call extract_api.bat
+call build_doc.bat
+POPD
+pause
+
+REM ----------------------------------------------------------------------------
+REM  Build Rototo
+REM ----------------------------------------------------------------------------
+
+call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+msbuild ..\projects\Visual2010\Rototo.sln /property:Configuration=Release /t:Clean;Rebuild
+
+pause
 
 echo delete old version directory
-rmdir versions /S /Q
-mkdir versions
+rmdir win32-version /S /Q
+mkdir win32-version
 
 pause 
 
-robocopy ../bin/win32/Release ./versions /e
-robocopy ../data/gamedata-sample ./versions/data/gamedata-sample /e
-robocopy ../doc/out ./versions/docs /e
-robocopy ../tools/ratata-distrib ./versions/tools /e
+robocopy ../bin/win32/Release ./win32-version /e
+robocopy ../data/gamedata-sample ./win32-version/data/gamedata-sample /e
+robocopy ../doc/out ./win32-version/docs /e
+robocopy ../tools/ratata-distrib ./win32-version/tools /e
 
 rem robocopy ../Rototo/source/doc ./trp settings.xml
 rem robocopy ../Rototo/source/doc ./trp version.txt
@@ -24,7 +36,7 @@ rem robocopy ../Rototo/rototo/themes ./trp/themes /e
 
 
 del  trp.zip
-7za a -tzip  trp.zip ./versions
+7za a -tzip  trp.zip ./win32-version
 rem "C:\Program Files (x86)\NSIS\makensis.exe" trp.nsi
 
 
