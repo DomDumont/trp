04459227ac29c99dbe787dada09bd8b8adde080b
DomDom <dom.dumont@gmail.com>
2014-09-18 17:05:23 +0200

    Cleaning ...

--- rototo/src/SoundManager.cpp
+++ rototo/src/SoundManager.cpp
@@ -32,130 +32,187 @@
 
 
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 SoundManager::SoundManager()
 	{
 	//TODO should be elsewhere
 
-	rate        = 22050;
-	//rate        = 44100;
-    format      = AUDIO_S16;
-    channels    = 2;
-    buffers     = 4096;
-	volume      = MIX_MAX_VOLUME;
-    volumeSFX   = MIX_MAX_VOLUME;
-	initialized = 0;
+	rate		= 22050;
+	//rate		= 44100;
+	format		= AUDIO_S16;
+	channels	= 2;
+	buffers		= 4096;
+	volume		= MIX_MAX_VOLUME;
+	volumeSFX	= MIX_MAX_VOLUME;
+	initialized	= 0;
 
 	}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 SoundManager::~SoundManager()
 	{
 	}
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void SoundManager::Init()
 {
-    if (Mix_OpenAudio(rate, format, channels, buffers) < 0)
-        {
-        SDL_Log("Couldn't open audio: %s\n", SDL_GetError());
-        }
-    else
-        {
-            Mix_QuerySpec(&rate, &format, &channels);
-            SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION,"Opened audio at %d Hz %d bit %s (%s), %d bytes audio buffer\n", rate,
-                           (format&0xFF),
-                           (channels > 2) ? "surround" : (channels > 1) ? "stereo" : "mono",
-                           (format&0x1000) ? "BE" : "LE",
-                           buffers );
-        }
-    
-    initialized = 1;
+	if (Mix_OpenAudio(rate, format, channels, buffers) < 0)
+	{
+	SDL_Log("Couldn't open audio: %s\n", SDL_GetError());
+	}
+	else
+	{
+		Mix_QuerySpec(&rate, &format, &channels);
+		SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION,"Opened audio at %d Hz %d bit %s (%s), %d bytes audio buffer\n", rate,
+			(format&0xFF),
+			(channels > 2) ? "surround" : (channels > 1) ? "stereo" : "mono",
+			(format&0x1000) ? "BE" : "LE",
+			buffers );
+	}
+	
+	initialized = 1;
 	Mix_VolumeMusic(volume);
 }
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void SoundManager::SetMusicVolume(int _newVolume)
 {
-    this->volume = _newVolume;
-    Mix_VolumeMusic(this->volume);
+	this->volume = _newVolume;
+	Mix_VolumeMusic(this->volume);
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void SoundManager::SetSFXVolume(int _newVolume)
 {
-    this->volumeSFX = _newVolume;
-    Mix_Volume(-1, this->volumeSFX);
+	this->volumeSFX = _newVolume;
+	Mix_Volume(-1, this->volumeSFX);
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void SoundManager::Shutdown()
 {
-    Mix_CloseAudio();
+	Mix_CloseAudio();
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void RegisterSoundManager()
 {
-    int r;
-    r = g_app->scriptManager->engine->RegisterGlobalFunction("void SND_SetMusicVolume(int _newVolume)", asMETHOD(SoundManager,SetMusicVolume), asCALL_THISCALL_ASGLOBAL, g_app->soundManager);SDL_assert(r>0);
- 	r = g_app->scriptManager->engine->RegisterGlobalFunction("void SND_SetSFXVolume(int _newVolume)", asMETHOD(SoundManager,SetSFXVolume), asCALL_THISCALL_ASGLOBAL, g_app->soundManager);SDL_assert(r>0);
-    
+	int r;
+	r = g_app->scriptManager->engine->RegisterGlobalFunction("void SND_SetMusicVolume(int _newVolume)", asMETHOD(SoundManager,SetMusicVolume), asCALL_THISCALL_ASGLOBAL, g_app->soundManager);SDL_assert(r>0);
+	r = g_app->scriptManager->engine->RegisterGlobalFunction("void SND_SetSFXVolume(int _newVolume)", asMETHOD(SoundManager,SetSFXVolume), asCALL_THISCALL_ASGLOBAL, g_app->soundManager);SDL_assert(r>0);
+
 }
+
 //-----------------------------------------------------------------------------
 // Music
 //-----------------------------------------------------------------------------
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void ConstructMusic(Music *thisPointer)
 {
 	new(thisPointer) Music();
 }
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void DestructMusic(Music *thisPointer)
 {
 	thisPointer->~Music();
 }
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void RegisterMusic()
 {
 	int r;
-    ///class:Music
+	///class:Music
 	r = g_app->scriptManager->engine->RegisterObjectType("Music", sizeof(Music), asOBJ_VALUE | asOBJ_APP_CLASS_CDK/*asOBJ_POD*/); SDL_assert(r>=0);
 	//this->RegisterClass<Music>("Music");
-    r = g_app->scriptManager->engine->RegisterObjectBehaviour("Music", asBEHAVE_CONSTRUCT,  "void f()",asFUNCTION(ConstructMusic), asCALL_CDECL_OBJLAST); SDL_assert(r>=0);
+	r = g_app->scriptManager->engine->RegisterObjectBehaviour("Music", asBEHAVE_CONSTRUCT,  "void f()",asFUNCTION(ConstructMusic), asCALL_CDECL_OBJLAST); SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectBehaviour("Music", asBEHAVE_DESTRUCT,   "void f()",asFUNCTION(DestructMusic),  asCALL_CDECL_OBJLAST);SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Music","void Load(string &in _file)", asMETHOD(Music, Load), asCALL_THISCALL);SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Music","void Play(int _nbLoops=-1, int _timeFadeIn=1000)", asMETHOD(Music, Play), asCALL_THISCALL);SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Music","void UnLoad()", asMETHOD(Music, UnLoad), asCALL_THISCALL);SDL_assert(r>=0);
-    r = g_app->scriptManager->engine->RegisterObjectMethod("Music","void Stop()", asMETHOD(Music, Stop), asCALL_THISCALL);SDL_assert(r>=0);
+	r = g_app->scriptManager->engine->RegisterObjectMethod("Music","void Stop()", asMETHOD(Music, Stop), asCALL_THISCALL);SDL_assert(r>=0);
 	/*
 	this->RegisterClassMethod("Music","void Load(string &in _file)", asMETHOD(Music, Load));
 	this->RegisterClassMethod("Music","void Play(int _nbLoops=-1, int _timeFadeIn=1000)", asMETHOD(Music, Play));
-    this->RegisterClassMethod("Music","void UnLoad()", asMETHOD(Music, UnLoad));
+	this->RegisterClassMethod("Music","void UnLoad()", asMETHOD(Music, UnLoad));
 	*/
 
 }
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 Music::Music()
-	{
+{
 	music = NULL;
-	}
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 Music::~Music()
+{
+if (music != NULL)
 	{
-	if (music != NULL)
-		{
-		Mix_FreeMusic(music);
-		music = NULL;
-		}
+	Mix_FreeMusic(music);
+	music = NULL;
 	}
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Music::UnLoad()
-    {
-	if (music != NULL)
-		{
-		Mix_FreeMusic(music);
-		music = NULL;
-		}
-    }
+{
+if (music != NULL)
+	{
+	Mix_FreeMusic(music);
+	music = NULL;
+	}
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Music::Load(const std::string & _file)
-	{        
+{
 	music = Mix_LoadMUS_RW(g_app->resourceManager->Load(_file,GAMEDATA|BOTH),1); //todo check this FreeSrc ?
-    
+
 	if ( music == NULL ) 
 		{
 		SDL_Log("Cannot load music %s %s",_file.c_str(),SDL_GetError());
@@ -164,25 +221,41 @@ void Music::Load(const std::string & _file)
 		{
 		SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION,"Music %s loaded sucessfully",_file.c_str());
 		}
-	}
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Music::Play(int _nbLoops, int _timeFadeIn)
 {
 	Mix_FadeInMusic(music,_nbLoops,_timeFadeIn);
 }
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void Music::Stop()
 {
-    Mix_HaltMusic();
+	Mix_HaltMusic();
 }
 //-----------------------------------------------------------------------------
 // Sound
 //-----------------------------------------------------------------------------
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void ConstructSound(Sound *thisPointer)
 {
 	new(thisPointer) Sound();
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void DestructSound(Sound *thisPointer)
 {
@@ -190,18 +263,30 @@ void DestructSound(Sound *thisPointer)
 }
 
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 Sound::Sound(): sample(NULL),channel(-1),volume(MIX_MAX_VOLUME)
 	{
 	}
 
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 Sound::~Sound()
-	{
+{
 	if (sample != NULL)
 		{
 		Mix_FreeChunk(sample);
 		sample = NULL;
 		}
-	}
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Sound::UnLoad()
 {
@@ -211,52 +296,71 @@ void Sound::UnLoad()
 		sample = NULL;
 		}
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 void Sound::Load(const std::string & _file)
 {
 	this->sample = Mix_LoadWAV_RW(g_app->resourceManager->Load(_file,GAMEDATA|BOTH),1); //todo check this FreeSrc ?
-    
+	
 	if ( sample == NULL ) 
 		{
 		SDL_Log("Cannot load sample %s %s",_file.c_str(),SDL_GetError());
-        return;
+		return;
 		}
 	SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION,"Sample %s loaded sucessfully",_file.c_str());
-    
-    Mix_VolumeChunk(this->sample,this->volume);
-
-
+	
+	Mix_VolumeChunk(this->sample,this->volume);
 
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Sound::Play(int _nbLoops)
 {
-    this->channel = Mix_PlayChannel(-1,this->sample,_nbLoops);
-    Mix_Volume(this->channel, g_app->soundManager->volumeSFX);
+	this->channel = Mix_PlayChannel(-1,this->sample,_nbLoops);
+	Mix_Volume(this->channel, g_app->soundManager->volumeSFX);
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Sound::Stop()
 {
-    Mix_HaltChannel(this->channel);
+	Mix_HaltChannel(this->channel);
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void Sound::SetVolume(int _newVolume)
 {
-    this->volume = _newVolume;
-    Mix_VolumeChunk(this->sample,this->volume);
+	this->volume = _newVolume;
+	Mix_VolumeChunk(this->sample,this->volume);
 }
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
 
 void RegisterSound()
 {
 	int r;
 
-    ///class:Sound
+	///class:Sound
 	r = g_app->scriptManager->engine->RegisterObjectType("Sound", sizeof(Sound) , asOBJ_VALUE | asOBJ_APP_CLASS_CDK/*asOBJ_POD*/); SDL_assert(r>=0);
-    r = g_app->scriptManager->engine->RegisterObjectBehaviour("Sound", asBEHAVE_CONSTRUCT,  "void f()",asFUNCTION(ConstructSound), asCALL_CDECL_OBJLAST); 
+	r = g_app->scriptManager->engine->RegisterObjectBehaviour("Sound", asBEHAVE_CONSTRUCT,  "void f()",asFUNCTION(ConstructSound), asCALL_CDECL_OBJLAST);
 	r = g_app->scriptManager->engine->RegisterObjectBehaviour("Sound", asBEHAVE_DESTRUCT,   "void f()",asFUNCTION(DestructSound),  asCALL_CDECL_OBJLAST);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void Load(string &in _file)", asMETHOD(Sound, Load), asCALL_THISCALL);SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void Play(int _nbLoops=0)", asMETHOD(Sound, Play), asCALL_THISCALL);SDL_assert(r>=0);
 	r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void UnLoad()", asMETHOD(Sound, UnLoad), asCALL_THISCALL);SDL_assert(r>=0);
-    r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void Stop()", asMETHOD(Sound, Stop), asCALL_THISCALL);SDL_assert(r>=0);
-    r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void SetVolume(int _newVolume)", asMETHOD(Sound, SetVolume), asCALL_THISCALL);SDL_assert(r>=0);
+	r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void Stop()", asMETHOD(Sound, Stop), asCALL_THISCALL);SDL_assert(r>=0);
+	r = g_app->scriptManager->engine->RegisterObjectMethod("Sound","void SetVolume(int _newVolume)", asMETHOD(Sound, SetVolume), asCALL_THISCALL);SDL_assert(r>=0);
 
 }
