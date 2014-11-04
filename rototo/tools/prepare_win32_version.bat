2eb63b5dbb45bf991169ead64ee877e9071429bc
MeyerAtWork <meyer.dumont@gmail.com>
2014-11-04 15:41:50 +0100

    Add android build to prepare_version.bat + rename android apk + update angelscript lib to 2.29.2

--- rototo/tools/prepare_win32_version.bat
+++ rototo/tools/prepare_win32_version.bat
@@ -1,8 +1,13 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+
+REM goto copy
+
 REM ----------------------------------------------------------------------------
 REM  Build Documentation
 REM ----------------------------------------------------------------------------
 REM activate this line to skip compilation goto tralala
+
+:generate_docs
 
 PUSHD .\doc
 call extract_api.bat
@@ -12,6 +17,7 @@ POPD
 REM ----------------------------------------------------------------------------
 REM  Build Ratata
 REM ----------------------------------------------------------------------------
+:compil_ratata_win32
 
 PUSHD .\ratata
 "C:\Qt\4.8.6\bin\qmake.exe" .\ratata.pro -r -spec win32-msvc2010 CONFIG+=release
@@ -23,6 +29,8 @@ POPD
 REM ----------------------------------------------------------------------------
 REM  Build Rototo Android
 REM ----------------------------------------------------------------------------
+
+:compil_rototo_android
 
 PUSHD ..\projects\android
 call ndk-build.cmd clean
@@ -36,11 +44,14 @@ REM ----------------------------------------------------------------------------
 REM  Build Rototo
 REM ----------------------------------------------------------------------------
 
+:compil_rototo_win32
+
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild ..\projects\Visual2010\Rototo.sln /property:Configuration=Release /t:Clean;Rebuild
+if ERRORLEVEL 1 goto compil_error
+pause
 
-
-:tralala
+:copy
 
 echo delete old version directory
 rmdir win32-version /S /Q
@@ -79,6 +90,8 @@ robocopy   ../doc/en ./win32-version README-WIN32.md
 robocopy   ../doc/fr ./win32-version LISEZMOI-WIN32.md 
 robocopy   ../doc ./win32-version VERSION.md 
 
+robocopy ../projects/android/bin ./win32-version/android/bin  Rototo-release-unsigned.apk
+
 rem robocopy ../data/gamedata-sample ./win32-version/data/gamedata-sample /e
 rem robocopy ../doc/out ./win32-version/docs /e
 rem robocopy ../tools/ratata-distrib ./win32-version/tools /e
@@ -97,5 +110,15 @@ cd ..
 
 "C:\Program Files (x86)\NSIS\makensis.exe" trp.nsi
 
+goto no_error
 
+:compil_error
 
+echo compil error
+pause
+goto end
+
+:no_error
+echo nice !
+
+:end
